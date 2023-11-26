e3d7ebf49111a01f902558c49674beef3eca45c3
rodriRoddl <104530048+rodriRoddl@users.noreply.github.com>
2023-11-26 17:30:21 -0300

    Agrega conexión con SW
    Se agrega la conexión con el SW para poder registrar las peticiones

--- proyetoIngesoft/api/views.py
+++ proyetoIngesoft/api/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Peticiones
+from .LengNaturalEspV2 import traducir
 
 # Create your views here.
 def index(request):
-    #return HttpResponse(documento)
+    if request.method == 'POST':
+        # Si la solicitud es un POST, significa que se envió un formulario
+        intencion = request.POST.get('peticion', '')  # Asegúrate de que 'peticion' coincida con el nombre del campo en tu formulario
+        if intencion:
+            
+            peticion = traducir(intencion)
+            # Guarda la nueva petición en la base de datos
+            nueva_peticion = Peticiones(peticion=peticion)
+            # Aplicar programa
+            nueva_peticion.save()
+            # Redirige a la página de historial o a donde desees después de guardar la petición
+            # return redirect('historial')
+
+    # Si la solicitud no es un POST o no se envió un formulario, simplemente renderiza la página principal
     return render(request, "index.html")
 
 def historial(request):
